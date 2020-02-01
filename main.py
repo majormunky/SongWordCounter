e1cbc5cf651c9cc15e56984406c8ed01234a0018
Josh Bright <josh.bright@gmail.com>
2020-01-31 20:17:16 -0800

    Setup a function to break up our lines into verse chunks

--- main.py
+++ main.py
@@ -9,10 +9,32 @@ def get_file_contents(filename):
     return result
 
 
+def categorize_lyrics(lines):
+    data = {}
+    current_key = None
+    for line in lines:
+        if line.startswith("["):
+            line_key = line.replace("[", "").replace("]", "")
+            line_key_parts = line_key.split(":")
+            if len(line_key_parts) == 2:
+                line_key = line_key_parts[1]
+            if line_key not in data.keys():
+                data[line_key] = []
+            current_key = line_key
+        elif line == "":
+            current_key = None
+            print("found blank line")
+        else:
+            data[current_key].append(line)
+    return data
+
+
 def main():
     filename = "data.txt"
     lines = get_file_contents(filename)
-    print(lines)
+    result = categorize_lyrics(lines)
+    for k, v in result.items():
+        print(k)
 
 
 if __name__ == "__main__":
